ed6acb2ad659112db31d653765801b7236455071
kimi <luojian2006@gmail.com>
2022-03-28 14:21:38 +0800

    commit

--- LongImageScroll/ViewController.swift
+++ LongImageScroll/ViewController.swift
@@ -8,10 +8,31 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    var bgImageView = UIImageView(image: UIImage(named: "bg"))
+    var bgImageView2 = UIImageView(image: UIImage(named: "bg"))
+    let ScreenHeight = UIScreen.main.bounds.size.height
+    let ScreenWidth = UIScreen.main.bounds.size.width
+    
+    func playAnimation(){
+        UIView.animate(withDuration: 10, delay: 0, options: [.repeat, .curveLinear]) {
+            self.bgImageView.frame.origin.y = -self.ScreenHeight*2
+            
+        }
+        UIView.animate(withDuration: 10, delay: 0, options: [.repeat, .curveLinear]) {
+            self.bgImageView2.frame.origin.y = 0
+        }
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        self.view.addSubview(bgImageView)
+        self.view.addSubview(bgImageView2)
+        bgImageView.frame = CGRect(x:0, y: 0, width: ScreenWidth, height: ScreenHeight*2)
+        bgImageView2.frame = CGRect(x:0, y: ScreenHeight*2, width: ScreenWidth, height: ScreenHeight*2)
+       playAnimation()
+
+        
     }
 
 
